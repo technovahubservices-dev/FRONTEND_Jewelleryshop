c4cd80114f300b0b0931d74f0c5a46bc9b4d3a44
technovahub2025 <technovahubcareer@gmail.com>
2026-09-02 15:01:58 +0530

    fix: Correct base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/test_jwellery/',
+
   plugins: [react()],
+
   test: {
     globals: true,
     environment: 'jsdom',
