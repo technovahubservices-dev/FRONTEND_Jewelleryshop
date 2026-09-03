22fa022dd4acd34b949ee8c8ad64d273563d0e1b
technovahubservices-dev <technovahubservices@gmail.com>
2026-09-03 14:25:57 +0530

    fix: correct indentation in vite configuration file

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:5000'
 
 export default defineConfig({
-  
+  base: "/",
   plugins: [react()],
   server: {
     port: 5173,
