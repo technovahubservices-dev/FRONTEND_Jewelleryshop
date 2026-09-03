b4d943d1842b70efbfd3246cbd4fd6da0c93712e
technovahubservices-dev <technovahubservices@gmail.com>
2026-09-03 12:05:22 +0530

    fix: update base path in vite configuration to include trailing slash

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:5000'
 
 export default defineConfig({
-  base : "/test_jwellery/", // 👈 Changed from "/test_jwellery" to "/test_jwellery/"
+  
   plugins: [react()],
   server: {
     port: 5173,
