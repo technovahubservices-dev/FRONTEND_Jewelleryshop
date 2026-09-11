7cc8e2a2a7fc3fb0db99ee326d7b828ebd467eb5
technovahubservices-dev <technovahubservices@gmail.com>
2026-09-11 15:52:52 +0530

    refactor: clean up vite.config.js by removing unused base and test configurations

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
-  base: '/test_jkr',
+  
   plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    setupFiles: './src/tests/setup.js',
+  
   },
-})
+)
