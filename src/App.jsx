a227ad2344f5e38674bc9d92981b053315bed242
technovahubservices-dev <technovahubservices@gmail.com>
2026-09-12 12:27:47 +0530

    fix: correct logical operator in BackButton rendering condition

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ export default function App() {
       <WishlistProvider>
         <div className="font-body-md text-on-background min-h-screen flex flex-col antialiased">
         {!isAdminRoute && <Header />}
-        {pathname !== '/' -and -not  && (<div className="max-w-container-max mx-auto w-full px-margin-desktop pt-4"><BackButton /></div>)}
+        {pathname !== '/' && !isAdminRoute && (<div className="max-w-container-max mx-auto w-full px-margin-desktop pt-4"><BackButton /></div>)}
         <AppRoutes />
         {!isAdminRoute && <Footer />}
       </div>
@@ -31,4 +31,6 @@ export default function App() {
   </AuthProvider>
   )
 }
+
+
 
