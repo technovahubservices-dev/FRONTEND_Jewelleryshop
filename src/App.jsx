6b7f6275d3786a62c073ee01156111d3df453d7c
technovahubservices-dev <technovahubservices@gmail.com>
2026-09-12 12:22:27 +0530

    feat: add BackButton component to App for improved navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useLocation } from 'react-router-dom'
 import Header from './components/layout/Header'
 import AppRoutes from './routes/AppRoutes'
 import Footer from './components/layout/Footer'
+import BackButton from './components/layout/BackButton'
 import { CartProvider } from './context/CartContext'
 import { WishlistProvider } from './context/WishlistContext'
 import { AuthProvider } from './context/AuthContext'
@@ -21,6 +22,7 @@ export default function App() {
       <WishlistProvider>
         <div className="font-body-md text-on-background min-h-screen flex flex-col antialiased">
         {!isAdminRoute && <Header />}
+        {pathname !== '/' -and -not  && (<div className="max-w-container-max mx-auto w-full px-margin-desktop pt-4"><BackButton /></div>)}
         <AppRoutes />
         {!isAdminRoute && <Footer />}
       </div>
@@ -29,3 +31,4 @@ export default function App() {
   </AuthProvider>
   )
 }
+
