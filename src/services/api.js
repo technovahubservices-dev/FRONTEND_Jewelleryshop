7345e9204653de11a5919dfa7de670a4c3587969
technovahubservices-dev <technovahubservices@gmail.com>
2026-09-22 18:43:33 +0530

    Revert "fix: ensure product price is a number in productAPI"
    This reverts commit f137cc2cb5d2639febf407ff69635c9a15d328e6.

--- src/services/api.js
+++ src/services/api.js
@@ -119,7 +119,7 @@ export const productAPI = {
     price:
       p.discountPrice > 0
         ? p.discountPrice
-        : Number(p.price || 0),
+        : p.price,
 
     originalPrice:
       p.price > 0 && p.discountPrice > 0
